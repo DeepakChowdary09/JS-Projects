f67fd57cb9bdb037ac488c45985e9bff33a47060
CodeVanguard <65644463+DeepakChowdary09@users.noreply.github.com>
2026-03-21 23:42:24 +0530

    Add files via upload

---
+++ Project1/script.js
@@ -0,0 +1,26 @@
+const fields = ["experience", "education","skills", "projects"];
+const maxScore =100;
+let atsScore =0;
+let resumeText ;
+
+   const  getScoreLabel=(score)=>{
+    if(score >=80) return "Strong";
+    else if (score >=60) return "Moderate";
+    else  return "Weak";
+    };
+   
+  
+
+// One-liner version (ternary):
+         // const     getScoreLabel =(score)>
+         //      score >=80?"Strong" :score>=60?"Moderate":"Weak";
+
+ resumeText = "I have experience in React and strong education";
+
+
+
+ resumeText = "I have experience in React and strong education";
+const getMissingFields = (resumeText, fields) =>
+  fields.filter((field) => !resumeText.includes(field));
+
+console.log(getMissingFields(resumeText, fields));
